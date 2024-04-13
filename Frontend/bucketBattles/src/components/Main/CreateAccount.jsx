0bf69d6dbba94d2501f03ff90444b6c67d988357
codydecoder <142354876+codydecoder@users.noreply.github.com>
2024-04-13 17:42:37 -0500

    Styled create account page

--- Frontend/bucketBattles/src/components/Main/CreateAccount.jsx
+++ Frontend/bucketBattles/src/components/Main/CreateAccount.jsx
@@ -20,9 +20,10 @@ const CreateAccount = ({ onSubmit }) => {
 
   return (
     <div className='border-2 border-black bg-gray-200 mx-auto p-4 m-0 border-t-0 border-b-0 min-h-full'>
-      <h2>Create Account</h2>
-      <form onSubmit={handleSubmit}>
+      <h2 className='m-2 mb-4 font-semibold text-2xl'>Create Account</h2>
+      <form className='space-x-2 space-y-2' onSubmit={handleSubmit}>
         <input
+          className='p-1 border-2 border-gray-400 rounded'
           type="text"
           placeholder="Username"
           value={username}
@@ -30,13 +31,14 @@ const CreateAccount = ({ onSubmit }) => {
           required
         />
         <input
+          className='p-1 border-2 border-gray-400 rounded'
           type="Password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
         />
-        <button type="submit">Create Account</button>
+        <button className='border-2 border-red-700 bg-red-500 rounded text-white p-1 m-1' type="submit">Create Account</button>
       </form>
     </div>
   )
