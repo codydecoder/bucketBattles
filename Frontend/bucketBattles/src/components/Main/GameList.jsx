14ef778f2829ca4b997089a69f0f06eb65f13e75
codydecoder <142354876+codydecoder@users.noreply.github.com>
2024-04-13 17:31:12 -0500

    Added button to gameList

--- Frontend/bucketBattles/src/components/Main/GameList.jsx
+++ Frontend/bucketBattles/src/components/Main/GameList.jsx
@@ -8,6 +8,15 @@ export default function GameList(){
 
     const [games, setGames] = useState([])
 
+    const addGameToPickList = async (gameId) => {
+        try {
+            const response = await axios.post(`${BASE_URL}/picks`, { game: gameId })
+            console.log('Game added to pick list:', response.data)
+        } catch (error) {
+            console.error('Error adding game to pick list:', error.message)
+        }
+    }
+    
     useEffect(() => {
         const getGames = async () => {
             try{
@@ -34,6 +43,7 @@ export default function GameList(){
                     <div className="border-2 border-red-600 bg-gray-400 rounded m-1" key={game._id}>
                             <h3 className="text-lg">Game: <span className='text-sm'>{game.homeTeam.name} vs {game.awayTeam.name}</span></h3>
                             <h3 className='text-lg'>Winner: <span className='text-sm'>{game.winningTeam ? game.winningTeam.name : 'TBD'}</span></h3>
+                            <button className='border-2 border-red-700 bg-red-500 rounded text-white p-1 m-1' onClick={() => addGameToPickList(game._id)}>Add Pick</button>
                     </div>
                 ))}
             </ul>
